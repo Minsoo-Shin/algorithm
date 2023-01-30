0ed14167c9b4c89e365feb4051306d0cf9e0e40e
Minsoo-Shin <alstn5038@gmail.com>
2023-01-30 09:00:36 +0900

    동일한 문제 한번 더 풀기

--- two_sum/twoSum.go
+++ two_sum/twoSum.go
@@ -1,7 +1,7 @@
 package two_sum
 
 /*
-	N^2 방법
+N^2 방법
 */
 func twoSum1(nums []int, target int) []int {
 	for i, vi := range nums {
@@ -18,11 +18,11 @@ func twoSum1(nums []int, target int) []int {
 }
 
 /*
-	시간복잡도 NlogN 방법
-	map으로 만든 다음, (n)
-	for문을 돌면서 합이 target이 되는 값을 찾아서 존재하면 반환한다. (n)
+시간복잡도 NlogN 방법
+map으로 만든 다음, (n)
+for문을 돌면서 합이 target이 되는 값을 찾아서 존재하면 반환한다. (n)
 
-	공간복잡도
+공간복잡도
 */
 func twoSum2(nums []int, target int) []int {
 	numsMap := make(map[int]int, 0)
@@ -41,8 +41,8 @@ func twoSum2(nums []int, target int) []int {
 }
 
 /*
-	leetcode의 어느 사람의 코드
-	for문을 한번만 돌면서 map[subtract] = index 를 만들어준다. 두번의 for 문을 돌지 않는다.
+leetcode의 어느 사람의 코드
+for문을 한번만 돌면서 map[subtract] = index 를 만들어준다. 두번의 for 문을 돌지 않는다.
 */
 func twoSum3(nums []int, target int) []int {
 	numsMap := make(map[int]int)
@@ -52,6 +52,22 @@ func twoSum3(nums []int, target int) []int {
 			return []int{i, j}
 		}
 		numsMap[vi] = i
+	}
+	return []int{}
+}
+
+/*
+내 코드
+*/
+func twoSum4(nums []int, target int) []int {
+	checkList := make(map[int]int)
+
+	for i, v := range nums {
+		diff := target - v
+		if diffIndex, ok := checkList[diff]; ok {
+			return []int{diffIndex, i}
+		}
+		checkList[v] = i
 	}
 	return []int{}
 }
